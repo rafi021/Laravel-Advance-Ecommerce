9ef5aa5dca9eaf63a05e574c57898e4148c852dd
rafi021 <mahmud.ibrahim021@gmail.com>
2021-06-30 12:34:40 +0600

    product tag show

--- resources/views/frontend/frontend_layout/widgets/product-tags-widget.blade.php
+++ resources/views/frontend/frontend_layout/widgets/product-tags-widget.blade.php
@@ -1,9 +1,24 @@
+@php
+    $tags_en = App\Models\Product::groupBy('product_tags_en')->select('product_tags_en')->get();
+    $tags_bn = App\Models\Product::groupBy('product_tags_bn')->select('product_tags_bn')->get();
+@endphp
 <div class="sidebar-widget product-tag wow fadeInUp">
-<h3 class="section-title">Product tags</h3>
-<div class="sidebar-widget-body outer-top-xs">
-    <div class="tag-list"> <a class="item" title="Phone" href="category.html">Phone</a> <a class="item active" title="Vest" href="category.html">Vest</a> <a class="item" title="Smartphone" href="category.html">Smartphone</a> <a class="item" title="Furniture" href="category.html">Furniture</a> <a class="item" title="T-shirt" href="category.html">T-shirt</a> <a class="item" title="Sweatpants" href="category.html">Sweatpants</a> <a class="item" title="Sneaker" href="category.html">Sneaker</a> <a class="item" title="Toys" href="category.html">Toys</a> <a class="item" title="Rose" href="category.html">Rose</a> </div>
-    <!-- /.tag-list --> 
+    <h3 class="section-title">Product tags</h3>
+    <div class="sidebar-widget-body outer-top-xs">
+        <div class="tag-list">
+            @if (session()->get('langiage') == 'bangla')
+            @foreach ($tags_en as $item)
+            <a class="item active" title="Phone" href="category.html">{{ str_replace(',',' ',$item->product_tags_en) }}</a>
+            @endforeach
+            @else
+            @foreach ($tags_bn as $item)
+                <a class="item active" title="Phone" href="category.html">{{ str_replace(',',' ',$item->product_tags_bn) }}</a>
+            @endforeach
+            @endif
+
+        </div>
+        <!-- /.tag-list -->
+    </div>
+    <!-- /.sidebar-widget-body -->
 </div>
-<!-- /.sidebar-widget-body --> 
-</div>
-<!-- /.sidebar-widget --> 
+<!-- /.sidebar-widget -->
