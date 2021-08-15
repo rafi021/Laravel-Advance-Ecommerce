fd1440fb6cea7792d3bcc2da1613ecb19091b2c4
rafi021 <mahmud.ibrahim021@gmail.com>
2021-08-15 18:16:21 +0600

    Cart Page Cart remove with Ajax

--- app/Http/Controllers/Frontend/CartPageController.php
+++ app/Http/Controllers/Frontend/CartPageController.php
@@ -3,17 +3,21 @@
 namespace App\Http\Controllers\Frontend;
 
 use App\Http\Controllers\Controller;
+use App\Models\Category;
 use Cart;
 use Illuminate\Http\Request;
+use Prophecy\Call\Call;
 
 class CartPageController extends Controller
 {
     public function myCartView()
     {
-        return view('frontend.frontend_layout.cart_page.mycart_view');
+        $categories = Category::with(['subcategory', 'subsubcategory', 'products'])->orderBy('category_name_en', 'ASC')->get();
+        return view('frontend.frontend_layout.cart_page.mycart_view', compact('categories'));
     }
     public function showmyCartList()
     {
+
         $carts = Cart::content();
         $cart_qty = Cart::count();
         $cart_total = Cart::total();
--- resources/views/frontend/frontend_layout/cart_page/mycart_view.blade.php
+++ resources/views/frontend/frontend_layout/cart_page/mycart_view.blade.php
@@ -9,14 +9,18 @@
 	<div class="container">
     <div class="my-wishlist-page">
         <div class="row">
-            <div class="col-md-12 my-wishlist">
+            <div class="col-md-12 shopping-cart-table">
                 <div class="table-responsive">
                     <table class="table">
                         <thead>
                             <tr>
-                                <th colspan="4" class="heading-title">My Cart
-                                    <strong>($<span id="total_bill"></span>)</strong>
-                                </th>
+                                <th>Image</th>
+                                <th>Name</th>
+                                <th>Color</th>
+                                <th>Size</th>
+                                <th>Qty</th>
+                                <th>Subtotal</th>
+                                <th>Remove</th>
                             </tr>
                         </thead>
                         <tbody id="cartPage">
@@ -42,11 +46,23 @@
                 var rows = ""
                 $.each(response.carts, function(key,value){
                     rows += `<tr>
-                                <td class="col-md-2"><img src="/${value.options.image} " alt="imga"></td>
-                                <td class="col-md-7">
+                                <td class="col-md-2"><img src="/${value.options.image} " alt="imga" style="width:60px; height:60px;"></td>
+                                <td class="col-md-2">
                                     <div class="product-name"><a href="#">${value.name}</a></div>
-                                    <div class="price">$${value.price} X ${value.qty} = $${value.price*value.qty}</div>
+                                    <div class="price">$${value.price}</div>
                                 </td>
+                                <td class="col-md-2">${value.options.color == null ? `<span>...</span>`:`<strong>${value.options.color}</strong>`}</td>
+
+                                <td class="col-md-2">${value.options.size == null ? `<span>...</span>`:`<strong>${value.options.size}</strong>`}</td>
+
+                                <td class="col-md-2">
+                                <button type="submit" class="btn btn-success btn-sm">+</button>
+                                <input type="text" value="${value.qty}" min="1" max="100" disabled="" style="width:25px;">
+                                <button type="submit" class="btn btn-success btn-sm">-</button>
+                                </td>
+
+                                <td class="col-md-2"><strong>$${value.subtotal}</strong></td>
+
                                 <td class="col-md-1 close-btn">
                                     <button type="submit" class="" id="${value.rowId}" onclick="cartRemove(this.id)"><i class="fa fa-times"></i></button>
                                 </td>
