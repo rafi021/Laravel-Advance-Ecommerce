2b90da5aa9d980e22ba5a0159b3a3a7c76237905
rafi021 <mahmud.ibrahim021@gmail.com>
2021-09-30 14:33:59 +0600

    Order & OrderItem Table Design

---
+++ database/migrations/2021_09_30_081511_create_orders_table.php
@@ -0,0 +1,59 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id');
+            $table->foreignId('division_id');
+            $table->foreignId('district_id');
+            $table->foreignId('state_id');
+            $table->string('name');
+            $table->string('email');
+            $table->string('phone');
+            $table->unsignedInteger('post_code')->nullable();
+            $table->text('notes')->nullable();
+            $table->string('payment_type');
+            $table->string('payment_method')->nullable();
+            $table->unsignedBigInteger('transaction_id');
+            $table->string('currency');
+            $table->float('amount', 8, 2);
+            $table->string('order_number');
+            $table->string('invoice_number');
+            $table->string('order_date');
+            $table->string('order_month');
+            $table->string('order_year');
+            $table->string('confirmed_date')->nullable();
+            $table->string('processing_date')->nullable();
+            $table->string('picked_date')->nullable();
+            $table->string('shipped_date')->nullable();
+            $table->string('delivered_date')->nullable();
+            $table->string('cancel_date')->nullable();
+            $table->string('return_date')->nullable();
+            $table->string('return_reason')->nullable();
+            $table->boolean('status')->default(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
---
+++ database/migrations/2021_09_30_081619_create_order_items_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrderItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('order_items', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('order_id')->constrained('orders')->onDelete('cascade');
+            $table->foreignId('product_id')->constrained('products');
+            $table->string('color');
+            $table->string('size');
+            $table->unsignedInteger('qty');
+            $table->float('unit_price',8,2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('order_items');
+    }
+}
